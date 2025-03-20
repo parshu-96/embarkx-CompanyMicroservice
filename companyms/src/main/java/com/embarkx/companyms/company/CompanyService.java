d3af66d43907e3294b58f77ba48367a9e71ea4cd
parshu <parshuram.m@accops.com>
2025-03-20 22:19:09 +0530

    Added code for Company Microservice

---
+++ companyms/src/main/java/com/embarkx/companyms/company/CompanyService.java
@@ -0,0 +1,12 @@
+package com.embarkx.companyms.company;
+
+
+import java.util.List;
+
+public interface CompanyService {
+    List<Company> getAllCompanies();
+    boolean updateCompany(Company updatedCompany,Long id);
+    void createCompany(Company company);
+    boolean deleteCompanyById(Long id);
+    Company getCompanyById(Long id);
+}
---
+++ companyms/src/main/java/com/embarkx/companyms/company/impl/CompanyServiceImpl.java
@@ -0,0 +1,58 @@
+package com.embarkx.companyms.company.impl;
+
+
+import com.embarkx.companyms.company.Company;
+import com.embarkx.companyms.company.CompanyRepository;
+import com.embarkx.companyms.company.CompanyService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+    private CompanyRepository companyRepository;
+
+    public CompanyServiceImpl(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @Override
+    public List<Company> getAllCompanies() {
+    return companyRepository.findAll();
+    }
+
+    @Override
+    public boolean updateCompany( Company updatedCompany,Long id) {
+        Optional<Company> companyOptional=companyRepository.findById(id);
+        if(companyOptional.isPresent()){
+            Company company=companyOptional.get();
+
+            company.setDescription(updatedCompany.getDescription());
+            company.setName(updatedCompany.getName());
+            companyRepository.save(company);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void createCompany(Company company) {
+        companyRepository.save(company);
+    }
+
+    @Override
+    public boolean deleteCompanyById(Long id) {
+        if(companyRepository.existsById(id)) {
+            companyRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Company getCompanyById(Long id) {
+        return companyRepository.findById(id).orElse(null);
+    }
+
+}
